ca25c1db2262c819b27c9621f181fe63c24c23cd
UncSald <133899575+UncSald@users.noreply.github.com>
2023-11-16 17:59:10 +0200

    Updated schema.sql
    Changed table format to make it easier to understand and connect.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
-CREATE TABLE Courses (id SERIAL PRIMARY KEY, name TEXT, course_id TEXT);
 CREATE TABLE Users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
-CREATE TABLE Course_grading (id SERIAL PRIMARY KEY, course_id integer REFERENCES Courses, material INTEGER, workload INTEGER, teacher INTEGER);
+CREATE TABLE Courses (course_id varchar PRIMARY KEY, name TEXT);
+CREATE TABLE Reviews (id SERIAL PRIMARY KEY, course varchar REFERENCES Courses, created TIMESTAMP);
+CREATE TABLE Teachers (id SERIAL PRIMARY KEY, name TEXT, grade INTEGER, review_id INTEGER REFERENCES Reviews);
+CREATE TABLE Material (id SERIAL PRIMARY KEY, grade INTEGER, review_id INTEGER REFERENCES Reviews);
+CREATE TABLE Workload (id SERIAL PRIMARY KEY, grade INTEGER, review_id INTEGER REFERENCES Reviews);
