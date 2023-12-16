f980316a62aaf56b3d633f77aeb20be170fd6eef
UncSald <133899575+UncSald@users.noreply.github.com>
2023-12-16 22:11:07 +0200

    Updated schema.sql
    Added some inserts to make testing the app more interesting. To see more the user must do their own review.

--- schema.sql
+++ schema.sql
@@ -17,4 +17,20 @@ CREATE TABLE Review_messages (id SERIAL PRIMARY KEY, review_id INTEGER REFERENCE
 --Add admin user. USERNAME: admin PASSWORD: kurssikarhu
 INSERT INTO Users (username, password, rights) VALUES ('admin',
 'scrypt:32768:8:1$LO4eIHwkM14dvFBD$c9c96ba44e4c6ab0f71bd7b1b7c6bc4146c2af0ea6c6d74fafe86f7fdf101d601aefeedaac0ccf483183b51e2c5a0dde68a16f71617b82d7205a5043491dbff1',
-'True')
+'True');
+
+--Add course tietokantojen perusteet and a review
+INSERT INTO Courses (course_id, name) VALUES ('TKT10004', 'TIETOKANTOJEN PERUSTEET');
+INSERT INTO Reviews (course, created) VALUES ('TKT10004', NOW());
+INSERT INTO Teachers (name, grade, review_id) VALUES ('ANTTI LAAKSONEN', 5, 1);
+INSERT INTO Material (grade, review_id) VALUES (4, 1);
+INSERT INTO Workload (grade, review_id) VALUES (3, 1);
+INSERT INTO Review_messages (review_id, message) VALUES (1, 'Rakastan tietokantoja! Nyt osaan myös käyttää niitä!');
+
+--Add course ohjelmoinnin perusteet and review
+INSERT INTO Courses (course_id, name) VALUES ('TKT10002', 'OHJELMOINNIN PERUSTEET');
+INSERT INTO Reviews (course, created) VALUES ('TKT10002', NOW());
+INSERT INTO Teachers (name, grade, review_id) VALUES ('ERKKI KAILA', 3, 2);
+INSERT INTO Material (grade, review_id) VALUES (5, 2);
+INSERT INTO Workload (grade, review_id) VALUES (2, 2);
+INSERT INTO Review_messages (review_id, message) VALUES (2, 'Rakastan ohjelmointia! Nyt osaan myös ohjelmoida!');
