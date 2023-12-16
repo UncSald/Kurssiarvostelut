7d534b00b253c98d4d6f9566ceee6c097824f1d4
UncSald <133899575+UncSald@users.noreply.github.com>
2023-12-16 20:51:23 +0200

    Updated schema.sql
    Added INSERT function adding an admin user so the app function will be easier to test.

--- schema.sql
+++ schema.sql
@@ -13,3 +13,8 @@ CREATE TABLE Teachers (id SERIAL PRIMARY KEY, name TEXT, grade FLOAT, review_id 
 CREATE TABLE Material (id SERIAL PRIMARY KEY, grade FLOAT, review_id INTEGER REFERENCES Reviews ON DELETE CASCADE);
 CREATE TABLE Workload (id SERIAL PRIMARY KEY, grade FLOAT, review_id INTEGER REFERENCES Reviews ON DELETE CASCADE);
 CREATE TABLE Review_messages (id SERIAL PRIMARY KEY, review_id INTEGER REFERENCES Reviews ON DELETE CASCADE, message TEXT);
+
+--Add admin user. USERNAME: admin PASSWORD: kurssikarhu
+INSERT INTO Users (username, password, rights) VALUES ('admin',
+'scrypt:32768:8:1$LO4eIHwkM14dvFBD$c9c96ba44e4c6ab0f71bd7b1b7c6bc4146c2af0ea6c6d74fafe86f7fdf101d601aefeedaac0ccf483183b51e2c5a0dde68a16f71617b82d7205a5043491dbff1',
+'True')
